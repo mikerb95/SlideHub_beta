aa6f6cdc29dbebc9a0f5f37b225d79b034e63da9
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-23 10:27:45 -0500

    chore: update Spring Boot version to 4.0.3, enhance test configuration with MockitoBean

--- src/test/java/com/syncslide/controller/ApiControllerTest.java
+++ src/test/java/com/syncslide/controller/ApiControllerTest.java
@@ -10,8 +10,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.bean.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
@@ -31,10 +31,10 @@ class ApiControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private SlideService slideService;
 
-    @MockBean
+    @MockitoBean
     private ConfigService configService;
 
     @BeforeEach
